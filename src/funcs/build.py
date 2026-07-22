3d0f957246c33b21193fea7bb7711cf670b9c3ce
Jaime Luis Mesa Viquez <jaimemesaviquez@gmail.com>
2026-07-22 14:24:45 +0200

    Generacion de feed rss implementado

--- src/funcs/build.py
+++ src/funcs/build.py
@@ -1,9 +1,11 @@
 import os
 import re
 import shutil
-from datetime import datetime
+from datetime import datetime, timezone
 from markdown2 import markdown
 from jinja2 import Environment, FileSystemLoader
+import xml.etree.ElementTree as ET
+from email.utils import format_datetime
 
 # Generate the index and posts on output
 def build():
@@ -30,12 +32,15 @@ def build():
             file_path = os.path.join("content", markdown_post)
 
             with open(file_path) as f:
-                post = markdown(f.read(), extras=['metadata'])
+                markdown_file = f.read()
+
+                post = markdown(markdown_file, extras=['metadata'])
 
                 posts.append({
                     "content" : post,
                     "slug" : markdown_post[:-3] + ".html",
                     "title" : post.metadata["title"],
+                    "description" : markdown_file.split("\n\n", 2)[1], 
                     "date" : post.metadata["date"]
                 })
 
@@ -64,8 +69,36 @@ def build():
             with open(file_path, 'w') as f:
                 f.write(rendered_post)
 
+        generate_rss(posts)
 
         # Render posts page
         print('Site generated succesfully')
     else:
         print('Error: eggs.toml not found in the current directory or ancestor')
+
+def generate_rss(posts):
+    site_url = "https://jaimemesa.neocities.org/"
+    rss = ET.Element("rss", version="2.0")    
+    channel = ET.SubElement(rss, "channel")
+
+    ET.SubElement(channel, "title").text = "Jaime's Jazz Joint"
+    ET.SubElement(channel, "description").text = "Jaime's favorite corner of the internet"
+    ET.SubElement(channel, "link").text = site_url
+
+    for post in posts:
+        item = ET.SubElement(channel, "item")
+
+        ET.SubElement(item, "title").text = post["title"]
+        ET.SubElement(item, "description").text = post["description"]
+        ET.SubElement(item, "link").text = site_url + "posts/" + post["slug"]
+
+        date = datetime.strptime(post["date"], "%d-%m-%Y")
+        date = date.replace(tzinfo=timezone.utc)
+
+        ET.SubElement(item, "pubDate").text = format_datetime(date)
+    
+    tree = ET.ElementTree(rss)
+    ET.indent(tree, space="  ")
+    tree.write("output/rss.xml", encoding="utf-8", xml_declaration=True)
+
+
